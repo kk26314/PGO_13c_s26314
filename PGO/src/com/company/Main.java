330791d504124d5c5a40b7d7672f390c8531a202
Kamil Kwiatkowski <s26314@pjwstk.edu.pl>
2022-03-13 17:54:31 +0100

    start

--- PGO/src/com/company/Main.java
+++ PGO/src/com/company/Main.java
@@ -5,14 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-	System.out.print("Podaj pierwszą liczbę całkowitą");
-	Scanner sc1 = new Scanner(System.in);
-	int a = sc1.nextInt();
 
-	System.out.print("Podaj drugą liczbę całkowitą");
-	Scanner sc2 = new Scanner(System.in);
-	int b = sc2.nextInt();
-		
-	System.out.print("Podaj trzecią liczbę całkowitą");
     }
 }
---
+++ PGO/src/com/company/cw1/zad1/zad1.java
@@ -0,0 +1,16 @@
+package com.company.cw1.zad1;
+import java.util.*;
+
+
+public class zad1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+
+
+
+
+    }
+
+}

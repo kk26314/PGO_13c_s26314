af76f8f6c476522dae8c7ec5811a1710f59c710b
Kamil Kwiatkowski <s26314@pjwstk.edu.pl>
2022-03-13 18:07:25 +0100

    drugi

--- PGO/src/com/company/cw1/zad1/zad1.java
+++ PGO/src/com/company/cw1/zad1/zad1.java
@@ -6,6 +6,9 @@ public class zad1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
 
 
 
